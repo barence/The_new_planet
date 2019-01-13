c6ef8cca92e8f005881c133220a55fee28cb301f
barence <44985626+barence@users.noreply.github.com>
2019-01-12 18:02:40 -0800

    initial commit

---
+++ Assets/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+
+    public Slider Healthbar;
+
+    void Start()
+    {
+        MaxHealth = 100f;
+        CurrentHealth = MaxHealth;
+        // Use this for initialization
+
+        Healthbar.value = CalculateHealth();
+    }
+
+    void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(10);
+        Healthbar.value = CalculateHealth();
+    }
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+    }
+
+    
+
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+}
---
+++ Assets/Walk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+    public AudioSource tickSource;
+
+    public float speed;
+
+    private Rigidbody2D rb2D;
+
+    // Use this for initialization
+    void Start() {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+
+        rb2D.AddForce(movement * speed);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        tickSource = GetComponent<AudioSource>();
+
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("apple"))
+        {
+            tickSource.Play();
+            other.gameObject.SetActive(false);
+        }
+    }
+}
