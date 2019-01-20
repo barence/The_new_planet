70e1139984b3e235bce8329e7a55fda57c93e81b
barence <44985626+barence@users.noreply.github.com>
2019-01-19 21:07:09 -0800

    added hunger

---
+++ Assets/Hunger.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hunger : MonoBehaviour
+{
+    public float CurrentHunger { get; set; }
+    public float MaxHunger { get; set; }
+
+    public Slider Hungerbar;
+
+    void Start()
+    {
+        MaxHunger = 100f;
+        CurrentHunger = MaxHunger;
+        // Use this for initialization
+
+        Hungerbar.value = CalculateHunger();
+    }
+
+    void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(10);
+        Hungerbar.value = CalculateHunger();
+    }
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHunger -= damageValue;
+    }
+
+
+
+
+    float CalculateHunger()
+    {
+        return CurrentHunger / MaxHunger;
+    }
+}
