2def5657f90247ab9851347b82a14b0c3261e924
barence <44985626+barence@users.noreply.github.com>
2019-04-06 17:21:30 -0700

    kind of quests

---
+++ Assets/arrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+
+    public float rotSpeed = 150f;
+
+    Transform larry;
+
+    void Update()
+    {
+        if (larry == null)
+        {
+
+           // GameObject.Find("player").transform.position + Vector3.right * 2;
+
+            GameObject go = GameObject.Find("larry");
+
+            if (go != null)
+            {
+                larry = go.transform;
+            }
+        }
+
+        if (larry == null)
+            return;
+
+        Vector3 dir = larry.position - transform.position;
+        dir.Normalize();
+
+        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 360;
+
+        Quaternion desiredRot = Quaternion.Euler(0, 0, zAngle);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+    }
+}
