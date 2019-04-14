9b9efba1d80e2f26f8f764b1f0f1593a5af59d7a
barence <44985626+barence@users.noreply.github.com>
2019-04-13 23:16:09 -0700

    quests

---
+++ Assets/postion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class postion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 new_position;
+        new_position = GameObject.Find("enemy").transform.position;
+        gameObject.transform.position = new_position;
+    }
+}
